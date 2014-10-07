bc6e4b30e19689d62d8a1444edae6733cd2c874f
monade <monade@gmail.com>
2014-10-07 08:49:08 -0700

    tried some known forms

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,6 @@
 module Main where
 
 import qualified Data.Map as Map
-import Data.Maybe
 import Control.Monad
 
 -- cell type
@@ -22,49 +21,54 @@ getNbh (x,y) = [(x-1,y-1), (x,y-1), (x+1,y-1), (x-1,y), (x+1,y), (x-1,y+1), (x,y
 -- evolution function
 evolve :: Coord -> Cell -> [(Coord, Cell)] -> Cell
 evolve _ cell nbh
-    | not cell  = c == 3 -- birth when 3 neighbours
-    | otherwise = c >= 2 && c <= 3 -- survival when between 2 and 3 neighbours
-    where 
-          c :: Int
-          c = sum' nbh -- count neighbours
-          sum' = foldr f 0
-          f (_,True) s = s+1
-          f _        s = s
+    -- birth when 3 neighbours
+    | not cell  = c == 3 
+    -- survival when between 2 and 3 neighbours
+    | otherwise = c >= 2 && c <= 3 where 
+        -- count neighbours
+        c :: Int
+        c = let f (_,True) s = s+1
+                f _        s = s
+            in foldr f 0 nbh 
 
 -- evolve cells and candidates one tick
 -- input: map of coordinate to pair of candidate flag and cell 
 -- the candidates flag indicates if a cell is checked for evolution in next tick
--- non candidates are just copied
--- a candidate is a cell which have changed or for which at least one neighbour has changed
+-- non candidates are not re-evaluated
+-- a candidate is a cell for which at least one neighbour has changed
 -- output: next generation map
--- promise: empty cells are candidates
+-- promise: empty cells in map are candidates
 nextTick :: Map.Map Coord (Bool, Cell) -> Map.Map Coord (Bool, Cell)
 -- fold previous map in new map starting with empty map
 nextTick cells = Map.foldrWithKey f Map.empty cells where
-    f coord (cand, cell) accCells = accCells' where
-        accCells'
-            -- just copy non-candidates, flag as candidate if already in accumulated map
-            | not cand = Map.alter g coord accCells
-            | not changed = cells2
-            | otherwise  = foldr h cells2 nbs
-        -- flag as candidate if in accumulated map
-        g x = Just (isJust x, cell)
-        cells2 
-            | newCell == emptyCell = accCells
-            | otherwise = Map.alter s coord accCells where
-                s Nothing = Just (changed, newCell)
-                s _       = Just (True, newCell)
-        -- insert as candidate cell if cell changed
-        h (coord2, _) = Map.alter j coord2
-        j Nothing = Just (True, emptyCell)
-        j (Just (_, c))    = Just (True, c)
-        -- new cell evolves from previous cell
-        newCell = evolve coord cell nbs
+    f coord (cand, cell) acc
+        -- unchanged and non-empty? see def
+        | not changed && cell /= emptyCell = acc2
+        -- changed? same as above plus all neighbours
+        | changed  = foldr f2 acc2 nbs
+        -- otherwise skip this cell
+        | otherwise = acc where
+            -- update accumulated cells
+            acc2 = Map.alter f3 coord acc where
+                f3 :: Maybe (Bool, Cell) -> Maybe (Bool, Cell)
+                -- insert as non-candidate when not already inserted
+                f3 Nothing = Just (False, newCell)
+                -- replace cell when found
+                f3 (Just (x, _)) = Just (x, newCell)
+            -- insert as candidate cell if cell changed
+            f2 (cand2, _) = Map.alter f3 cand2 where
+                f3 Nothing       = Just (True, emptyCell)
+                f3 (Just (_, x)) = Just (True, x)
+            -- new cell evolves from previous cell
+            (newCell, changed)
+                | not cand  = (cell, False)
+                | otherwise = (cell2, changed2) where
+                    cell2    = evolve coord cell nbs
+                    changed2 = cell2 /= cell
         -- build neighbourhood list as (coord, cell) pairs
-        nbs = map i $ getNbh coord
-        -- find cell from coord
-        i coord'  = (coord', snd (Map.findWithDefault (False, emptyCell) coord' cells))
-        changed = cell /= newCell
+            nbs = map f3 $ getNbh coord where
+                -- find cell from coord
+                f3 coord2  = (coord2, snd (Map.findWithDefault (False, emptyCell) coord2 cells))
         
 -- initialize cell map from cell list
 initialize :: [Coord] -> Map.Map Coord (Bool, Cell)
@@ -84,11 +88,12 @@ getFrame :: Map.Map Coord (Bool, Cell) -> (Int, Int, Int, Int)
 getFrame cells 
     | cells == Map.empty = (0, 0, 0, 0)
     | otherwise          = (minX, minY, maxX, maxY) where
-    minX = minimum $ fst $ unzip $ Map.keys cells
-    minY = minimum $ snd $ unzip $ Map.keys cells
-    maxX = maximum $ fst $ unzip $ Map.keys cells
-    maxY = maximum $ snd $ unzip $ Map.keys cells
-
+        minX = minimum $ fst pair
+        minY = minimum $ snd pair
+        maxX = maximum $ fst pair
+        maxY = maximum $ snd pair
+        pair = unzip $ Map.keys cells
+    
 -- draw cells
 draw :: Map.Map Coord (Bool, Cell) -> String
 draw cells = header ++ pic where
@@ -111,10 +116,14 @@ draw cells = header ++ pic where
 main :: IO ()
 main = do
     putStrLn "Game of life"
-    let cells = initialize [(0,2),(1,2),(2,2),(2,1),(1,0)] 
-    f cells where
-        f :: Map.Map Coord (Bool, Cell) -> IO ()
-        f cells = do
+    --let floater = initialize [(0,2),(1,2),(2,2),(2,1),(1,0)] 
+    --let block = initialize [(0,0),(0,1),(1,0),(1,1)]
+    --let blinker = initialize [(0,0), (0,1),(0,2)]
+    let rpentomino = initialize [(1,0),(2,0),(0,1),(1,1),(1,2)]
+    f rpentomino 0 where
+        f :: Map.Map Coord (Bool, Cell) -> Int -> IO ()
+        f cells n = do
+            print n
             putStr $ draw cells
             input <- getChar
-            unless (input == 'q') $ f $ nextTick cells
+            unless (input == 'q') $ f (nextTick cells) (n+1)
