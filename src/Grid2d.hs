3cdbff125fe68d85e64b02d8bec041d4165dc9b3
monade <monade@gmail.com>
2014-12-31 13:45:58 +0100

    added mouse select and deselect cells

--- src/Grid2d.hs
+++ src/Grid2d.hs
@@ -1,4 +1,6 @@
 import Graphics.UI.GLUT 
+import Data.IORef
+import qualified Data.Map.Lazy as LMap
 
 -- coordinate >= 0 to position in [-1,1]
 c2p :: Fractional a => a -> a -> a
@@ -21,57 +23,68 @@ drawRect r n x y = rect v1 v2 where
 -- draw grid n > 0
 drawGrid :: Int -> IO ()
 drawGrid n = renderPrimitive Lines $
-   mapM_ (\(x, y) -> vertex $ Vertex2 x y) (hLines ++ vLines) where
-      hLines = [ (x  , f y) | y <- [0..rn], x <- [-1,1]]
-      vLines = [ (f x, y  ) | x <- [0..rn], y <- [-1,1]]
-      f r    = c2p r rn
-      rn     = fromIntegral n :: GLfloat
+   mapM_ (\(x, y) -> vertex $ Vertex2 x y) (hl ++ vl) where
+      hl = [ (x  , f y) | y <- [0..rn], x <- [-1,1]]
+      vl = [ (f x, y  ) | x <- [0..rn], y <- [-1,1]]
+      f r = c2p r rn
+      rn  = fromIntegral n :: GLfloat
+
+type MapRef = IORef (LMap.Map (Int,Int) Bool)
 
 main :: IO ()
 main = do
+  -- reference var for set grid cells
+  cs <- newIORef LMap.empty :: IO MapRef 
   -- grid size
-  let n  = 10   :: Int
+  let n = 10 :: Int
   -- grid spacing for cell fill
-  let r  = 0.01 :: GLfloat
+  let r = 0.01 :: GLfloat
   -- window caption
-  let c  = "2 Dim Grid"
+  let c = "2 Dim Grid"
+  -- initialize app and window
   (_progName, _args) <- getArgsAndInitialize
   _window            <- createWindow c
-  displayCallback $= display r n [] 
+  -- set callbacks
+  displayCallback $= display r n cs 
   reshapeCallback $= Just reshape 
-  mouseCallback   $= Just (mouse r n)
+  mouseCallback   $= Just (mouse n cs)
+  -- enter mainloop
   mainLoop
 
 -- reshape with constraints on minimum size and quadratic aspect ratio
 reshape :: ReshapeCallback
 reshape s@(Size x y) = do 
   Size sx sy <- get screenSize
-  let m = max (min x y) (min (div sx 10) (div sy 10))
-  let n = Size m m 
-  windowSize $= n
-  viewport   $= (Position 0 0, s)
+  windowSize $= f sx sy 
+  viewport   $= (Position 0 0, s) where
+    f sx sy = Size m m where
+      m = max (min x y) (min (div sx 10) (div sy 10))
 
 -- mouse event callback, fill grid cell on left click
 -- r >= 0: cell fill spacing
 -- n > 0:  grid size
-mouse :: GLfloat -> Int -> MouseCallback
-mouse r n LeftButton Down (Position px py) = do
-  -- calc pos in [-1,1] from mouse click pos
+mouse :: Int -> MapRef -> MouseCallback
+mouse n cs LeftButton Down (Position px py) = do
   Size sx sy <- get windowSize
-  let x  = -1 + 2 * (fromIntegral px / fromIntegral sx)
-  let y  =  1 - 2 * (fromIntegral py / fromIntegral sy)
   -- calc coord from pos
-  let rn = fromIntegral n :: GLfloat
-  let cx = p2c x rn
-  let cy = p2c y rn
-  -- fill cell
-  drawRect r n cx cy 
-  flush 
+  modifyIORef cs $ LMap.alter h (g px py sx sy)
+  postRedisplay Nothing where
+    h Nothing = Just True
+    h _       = Nothing
+    g x y sx sy = (f u, f v) where 
+      -- calc pos in [-1,1] from mouse click pos
+      u = -1+2*x'/sx'
+      v =  1-2*y'/sy'
+      -- convert integrals to floats
+      [x',y',sx',sy'] = map fromIntegral [x,y,sx,sy] :: [GLfloat]
+      f               = flip p2c $ fromIntegral n
 mouse _ _ _ _ _ = return ()
 
-display :: GLfloat -> Int -> [(Int,Int)] -> DisplayCallback
+-- display grid and set cells
+display :: GLfloat -> Int -> MapRef -> DisplayCallback
 display r n cs = do 
   clear [ColorBuffer]
   drawGrid n  
-  mapM_ (uncurry $ drawRect r n) cs 
+  m <- get cs 
+  mapM_ (uncurry $ drawRect r n) (LMap.keys m) 
   flush
