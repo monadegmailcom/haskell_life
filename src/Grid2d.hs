58b29ecd80f5369ff3100ba93fb46fd9f1c0cb32
monade <monade@gmail.com>
2015-01-03 16:23:06 +0100

    changed resizing window semantics

--- src/Grid2d.hs
+++ src/Grid2d.hs
@@ -1,90 +1,133 @@
 import Graphics.UI.GLUT 
 import Data.IORef
-import qualified Data.Map.Lazy as LMap
+import qualified Data.Set as Set 
+import Control.Monad (liftM)
 
--- coordinate >= 0 to position in [-1,1]
-c2p :: Fractional a => a -> a -> a
-c2p c n = -1+2*c/n
+-- coordinate to viewport position 
+-- (0,0) -> (-1,1), (n,n) -> (1,-1)
+c2p :: (Int,Int) -> (Int,Int) -> (GLfloat,GLfloat)
+c2p (cx,cy) (n,k) = (f cx n,-f cy k) where
+  f c m = -1+2*c'/m' where
+    c' = fromIntegral c
+    m' = fromIntegral m
 
--- position in [0,1] to coordinate >= 0
-p2c :: RealFrac a => a -> a -> Int 
-p2c p n = floor $ n*(p+1)/2
+-- viewport position to coordinate 
+p2c :: (GLfloat,GLfloat) -> (Int,Int) -> (Int,Int)
+p2c (px,py) (n,k) = (f px n,k-1-f py k) where
+  f p m = floor $ m'*(p+1)/2 where
+    m' = fromIntegral m
 
--- fill grid n >= 0 and cell (x,y) with spacing r >= 0
-drawRect :: GLfloat -> Int -> Int -> Int -> IO ()
-drawRect r n x y = rect v1 v2 where
-   v1  = Vertex2 ( r + f rx    ) ( r + f ry    )
-   v2  = Vertex2 (-r + f (rx+1)) (-r + f (ry+1))
-   f t = c2p t rn
-   rx  = fromIntegral x
-   ry  = fromIntegral y
-   rn  = fromIntegral n
-
--- draw grid n > 0
-drawGrid :: Int -> IO ()
-drawGrid n = renderPrimitive Lines $
+-- fill cell with spacing 
+drawRect :: (Int,Int) -> (Int,Int) -> IO () 
+drawRect d c@(cx,cy) = rect v1 v2 where
+   v1 = uncurry Vertex2 $ c2p c d
+   v2 = uncurry Vertex2 $ c2p (cx+1,cy+1) d
+   
+-- draw grid 
+drawGrid :: (Int,Int) -> IO ()
+drawGrid d@(n,k) = renderPrimitive Lines $
    mapM_ (\(x, y) -> vertex $ Vertex2 x y) (hl ++ vl) where
-      hl = [ (x  , f y) | y <- [0..rn], x <- [-1,1]]
-      vl = [ (f x, y  ) | x <- [0..rn], y <- [-1,1]]
-      f r = c2p r rn
-      rn  = fromIntegral n :: GLfloat
+      hl = [ c2p (x,y) d | y <- [0..k], x <- [0,n]]
+      vl = [ c2p (x,y) d | x <- [0..n], y <- [0,k]]
 
-type MapRef = IORef (LMap.Map (Int,Int) Bool)
+-- cell coordinate map
+type Coords = Set.Set (Int,Int) 
+
+toSize :: Int -> Int -> Size
+toSize x y = Size (fromIntegral x) (fromIntegral y)
+fromSize :: Size -> (Int,Int)
+fromSize (Size x y) = (fromIntegral x,fromIntegral y)
+fromPosition :: Position -> (Int,Int)
+fromPosition (Position x y) = (fromIntegral x,fromIntegral y)
 
 main :: IO ()
 main = do
-  -- reference var for set grid cells
-  cs <- newIORef LMap.empty :: IO MapRef 
-  -- grid size
-  let n = 10 :: Int
-  -- grid spacing for cell fill
-  let r = 0.01 :: GLfloat
-  -- window caption
-  let c = "2 Dim Grid"
-  -- initialize app and window
+  -- initialize app 
   (_progName, _args) <- getArgsAndInitialize
-  _window            <- createWindow c
+
+  -- create window caption
+  let cCaption = "Conways's game of life"
+  _window <- createWindow cCaption
+
+  -- get current window size
+  Size wx wy <- get windowSize
+
+  -- default number of cells in x dim
+  let cdefX = 10 
+
+  -- get number of cells in x and y dim and width in pixels
+  let (n,k,w) = getDim cdefX wx wy :: (Int,Int,Int)
+      
+  -- resize current window
+  windowSize $= toSize (n*w) (k*w) 
+
+  -- reference var for grid dimension 
+  dim <- newIORef (n,k,w) :: IO (IORef (Int,Int,Int))
+
+  -- reference var for grid cell set
+  cs <- newIORef Set.empty :: IO (IORef Coords)
+
   -- set callbacks
-  displayCallback $= display r n cs 
-  reshapeCallback $= Just reshape 
-  mouseCallback   $= Just (mouse n cs)
+  displayCallback $= display dim cs 
+  reshapeCallback $= Just (reshape dim)
+  mouseCallback   $= Just (mouse dim cs)
+
   -- enter mainloop
-  mainLoop
+  mainLoop 
 
--- reshape with constraints on minimum size and quadratic aspect ratio
-reshape :: ReshapeCallback
-reshape s@(Size x y) = do 
-  Size sx sy <- get screenSize
-  windowSize $= f sx sy 
-  viewport   $= (Position 0 0, s) where
-    f sx sy = Size m m where
-      m = max (min x y) (min (div sx 10) (div sy 10))
+  where
+    getDim n wx wy = (n',k,w) where
+      k = max 1 $ div (n'*wy') wx' 
+      w = max 1 $ div wx' n' 
+      wx' = fromIntegral $ max 1 wx 
+      wy' = fromIntegral wy
+      n'  = max 1 n
 
--- mouse event callback, fill grid cell on left click
--- r >= 0: cell fill spacing
--- n > 0:  grid size
-mouse :: Int -> MapRef -> MouseCallback
-mouse n cs LeftButton Down (Position px py) = do
-  Size sx sy <- get windowSize
+-- resize dim and window 
+reshape :: IORef (Int,Int,Int) -> ReshapeCallback
+reshape dim s = do 
+  -- resize dim according to resized window
+  modifyIORef dim f
+
+  -- resize window accoring to dim
+  (n,k,w)    <- get dim
+  windowSize $= toSize (n*w) (k*w) 
+
+  -- set new viewport
+  viewport   $= (Position 0 0, s) 
+
+  where
+    f (_,_,w) = (g sx,g sy,w) where
+      g = max 1 . flip div w
+      (sx,sy) = fromSize s
+
+-- mouse event callback, toggle grid cell on left click
+mouse :: IORef (Int,Int,Int) -> IORef Coords -> MouseCallback
+mouse dim cs LeftButton Down pos = do
   -- calc coord from pos
-  modifyIORef cs $ LMap.alter h (g px py sx sy)
-  postRedisplay Nothing where
-    h Nothing = Just True
-    h _       = Nothing
-    g x y sx sy = (f u, f v) where 
-      -- calc pos in [-1,1] from mouse click pos
-      u = -1+2*x'/sx'
-      v =  1-2*y'/sy'
-      -- convert integrals to floats
-      [x',y',sx',sy'] = map fromIntegral [x,y,sx,sy] :: [GLfloat]
-      f               = flip p2c $ fromIntegral n
+  s <- liftM fromSize $ get windowSize
+  let w = fromPosition pos
+  (n,k,_) <- get dim
+  modifyIORef cs $ f (n,k) s w 
+  
+  postRedisplay Nothing 
+  
+  where
+    -- toggle cell membership in set
+    f d s w set   
+      | Set.member c set = Set.delete c set  
+      | otherwise        = Set.insert c set where
+        c = p2c p d
+        -- calc viewport pos from mouse click pos
+        p = c2p w s 
 mouse _ _ _ _ _ = return ()
 
 -- display grid and set cells
-display :: GLfloat -> Int -> MapRef -> DisplayCallback
-display r n cs = do 
+display :: IORef (Int,Int,Int) -> IORef Coords -> DisplayCallback
+display dim cs = do 
   clear [ColorBuffer]
-  drawGrid n  
-  m <- get cs 
-  mapM_ (uncurry $ drawRect r n) (LMap.keys m) 
+  d <- liftM (\(n,k,_) -> (n,k)) $ get dim
+  drawGrid d 
+  s <- get cs 
+  mapM_ (drawRect d) (Set.elems s) 
   flush
